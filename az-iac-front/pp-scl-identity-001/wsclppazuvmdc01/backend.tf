c4c4de116eb496b26bf3422a5970f34e9d5dcc45
asanchezd8_bch <asanchezd8@bch.bancodechile.cl>
2025-09-17 21:19:02 -0300

    feat: VM-DC-Update

--- az-iac-front/pp-scl-identity-001/wsclppazuvmdc01/backend.tf
+++ az-iac-front/pp-scl-identity-001/wsclppazuvmdc01/backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  backend "azurerm" {    
+  backend "azurerm" {
     subscription_id      = "67e8f9c1-a3d9-405e-b72d-43e7c16a46d3"
     resource_group_name  = "AR-TFstate"
     storage_account_name = "sttf1727848146"
